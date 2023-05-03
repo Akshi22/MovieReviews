3a12a2eb1b84f9098edbec2432905e1f814aeadb
Akshitha Kumbam <69581478+Akshi22@users.noreply.github.com>
2023-05-03 19:32:40 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#used to treat all the directories with this __ini__.py as a package
+# The __init__.py files are required to make Python treat directories containing the file as packages. 
+# This prevents directories with a common name, such as string , from unintentionally hiding valid modules that occur later on the module search path
+
+# here the directory is the src. src is a directory and inside src is the file named __init__.py
