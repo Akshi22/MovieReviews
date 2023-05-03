75270a832257d9b7d1fa1d4dabdbaa14681edc06
Akshitha Kumbam <69581478+Akshi22@users.noreply.github.com>
2023-05-03 19:31:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+#Used to create a ML application as a package
+from setuptools import setup, find_packages #find_packages finds all the packages in the entire ML library
 
+#setup is the metadata information about the entire project
+setup(
+  name = 'Akshi NLP project',
+  version = '0.0.1', # can update the versions as it goes forward
+  author = 'Akshi',
+  author_email = 'akshitha.3022@gmail.com',
+  packages = find_packages(),
+  install_requires = ['pandas','numpy','seaborn'],
+  
+  
+
+)
